9b108b80dd428a16a076d89ec26acd8c4586ca6b
leandrosiq <le_kts12@hotmail.com>
2021-07-27 01:18:13 -0300

    alterando header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-  background: var(--shape);
+  border-bottom: 1px solid var(--shape);
 
   div {
     max-width: 1020px;
